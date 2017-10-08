5b3c2144efe10a7fc7afe8226a8fb9ab6e3e6033
vousavez <32397584+vousavez@users.noreply.github.com>
2017-10-08 13:20:41 -0300

    Update hello.py
    python3 support

--- hello.py
+++ hello.py
@@ -3,8 +3,8 @@
 import quadratic
 
 def main():
-    print "Sveikas, Pasauli!"
-    print "phi =", quadratic.solve_quadratic(1, -1, -1)[1]
+    print("Sveikas, Pasauli!")
+    print("phi =", quadratic.solve_quadratic(1, -1, -1)[1])
 
 if __name__ == "__main__":
     main()
